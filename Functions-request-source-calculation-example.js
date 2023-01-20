620e2d19df055e2670d49d0ebf637bbe64136634
CL-Victor <victor@smartcontract.com>
2023-01-20 16:47:35 +0800

    Update args to [2] (S22 Leaderboard)

--- Functions-request-source-calculation-example.js
+++ Functions-request-source-calculation-example.js
@@ -2,7 +2,7 @@
 // This leaderboard could trigger on-chain rewards transfers to the winners in the form of NFTs (unique tradeable in-game skins) or cryptocurrency payments
 
 // Arguments can be provided when a request is initated on-chain and used in the request source code as shown below
-const fortnite_season = args[0]
+const fortnite_season = args[2]
 let leaderboard = "";
 const fortniteLeaderboard = await Functions.makeHttpRequest({
   url: `https://www.epicgames.com/fortnite/competitive/api/leaderboard/floating/Hype_${fortnite_season}/undefined`
